3ebcfd4f89bad2a6cee7690a3d8f88cfba2492f8
yann dupis <yann.dupis@gmail.com>
2020-05-22 12:10:20 -0700

    add license for testing executors utils

--- tff_aggregations/channels_test_utils.py
+++ tff_aggregations/channels_test_utils.py
@@ -1,3 +1,20 @@
+  
+# Lint as: python3
+# Copyright 2019, The TensorFlow Federated Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Utils for testing executors."""
+
 from absl.testing import absltest
 from absl.testing import parameterized
 
@@ -9,7 +26,6 @@ from tensorflow_federated.python.core.impl.executors import federating_executor
 from tff_aggregations import channels
 
 
-# NOTE AsycTestCase should be imported from tff
 class AsyncTestCase(absltest.TestCase):
   """A test case that manages a new event loop for each test.
 
