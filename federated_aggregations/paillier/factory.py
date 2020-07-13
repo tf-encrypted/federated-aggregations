9191ef1e8b32987f4125726c7e7893adb65715b7
jvmancuso <jason@manc.us>
2020-07-13 18:48:31 -0400

    refresh executor factory logic

--- federated_aggregations/paillier/factory.py
+++ federated_aggregations/paillier/factory.py
@@ -1,9 +1,15 @@
 import functools
+from typing import List, Callable, Optional, Sequence
 
 import tensorflow as tf
 import tensorflow_federated as tff
 from tensorflow_federated.python.common_libs import py_typecheck
+from tensorflow_federated.python.core.impl.executors import eager_tf_executor
+from tensorflow_federated.python.core.impl.executors import executor_base
+from tensorflow_federated.python.core.impl.executors import executor_factory
 from tensorflow_federated.python.core.impl.executors import executor_stacks
+from tensorflow_federated.python.core.impl.executors import federating_executor
+from tensorflow_federated.python.core.impl.executors import sizing_executor
 from tensorflow_federated.python.core.impl.types import placement_literals
 
 from federated_aggregations import channels
@@ -24,109 +30,137 @@ from federated_aggregations.paillier import computations as paillier_comp
 #   - composite executory factory
 #   - worker pool factory (for use with RemoteExecutor)
 
+class AggregatingUnplacedExecutorFactory(executor_stacks.UnplacedExecutorFactory):
+
+  def __init__(
+      self,
+      *,
+      use_caching: bool,
+      server_device: Optional[tf.config.LogicalDevice] = None,
+      aggregator_device: Optional[tf.config.LogicalDevice] = None,
+      client_devices: Optional[Sequence[tf.config.LogicalDevice]] = ()):
+    super().__init__(
+        use_caching=use_caching,
+        server_device=server_device,
+        client_devices=client_devices)
+    self._aggregator_device = aggregator_device
+
+  def create_executor(
+      self,
+      *,
+      cardinalities: Optional[executor_factory.CardinalitiesType] = None,
+      placement: Optional[placement_literals.PlacementLiteral] = None
+  ) -> executor_base.Executor:
+    if cardinalities:
+      raise ValueError(
+          'Unplaced executors cannot accept nonempty cardinalities as '
+          'arguments. Received cardinalities: {}.'.format(cardinalities))
+    if placement == paillier_placement.PAILLIER:
+      ex = eager_tf_executor.EagerTFExecutor(device=self._aggregator_device)
+      return executor_stacks._wrap_executor_in_threading_stack(ex)
+    return super().create_executor(
+        cardinalities=cardinalities, placement=placement)
+
+
+class PaillierAggregatingExecutorFactory(executor_stacks.FederatingExecutorFactory):
+
+  def create_executor(
+      self, cardinalities: executor_factory.CardinalitiesType
+  ) -> executor_base.Executor:
+    """Constructs a federated executor with requested cardinalities."""
+    num_clients = self._validate_requested_clients(cardinalities)
+    client_stacks = [
+        self._unplaced_executor_factory.create_executor(
+            cardinalities={}, placement=placement_literals.CLIENTS)
+        for _ in range(self._num_client_executors)
+    ]
+    if self._use_sizing:
+      client_stacks = [
+          sizing_executor.SizingExecutor(ex) for ex in client_stacks
+      ]
+      self._sizing_executors.extend(client_stacks)
+    paillier_stack = self._unplaced_executor_factory.create_executor(
+        cardinalities={}, placement=paillier_placement.PAILLIER)
+    if self._use_sizing:
+      paillier_stack = sizing_executor.SizingExecutor(paillier_stack)
+    # Set up secure channel between clients & Paillier executor
+    secure_channel_grid = channels.ChannelGrid({
+      (tff.CLIENTS,
+      # TODO: replace this line with the one after it
+       paillier_placement.PAILLIER): channels.PlaintextChannel,
+      #  paillier_placement.PAILLIER): channels.EasyBoxChannel,
+      (tff.CLIENTS, 
+       tff.SERVER): channels.PlaintextChannel,
+      (paillier_placement.PAILLIER, 
+       tff.SERVER): channels.PlaintextChannel})
+    # Build a FederatingStrategy factory for Paillier aggregation with the secure channel setup
+    strategy_factory = paillier_strategy.PaillierAggregatingStrategy.factory(
+        {
+            placement_literals.CLIENTS: [
+                client_stacks[k % len(client_stacks)]
+                for k in range(num_clients)
+            ],
+            placement_literals.SERVER:
+                self._unplaced_executor_factory.create_executor(
+                    cardinalities={}, placement=placement_literals.SERVER),
+            paillier_placement.PAILLIER: paillier_stack,
+        },
+        channel_grid=secure_channel_grid,
+        # NOTE: we let the server generate it's own key here, but for proper
+        # deployment we would want to supply a key verified by proper PKI
+        key_inputter=paillier_comp.make_keygen(bitlength=2048))
+    unplaced_executor = self._unplaced_executor_factory.create_executor(
+        cardinalities={})
+    executor = federating_executor.FederatingExecutor(
+        strategy_factory, unplaced_executor)
+    return executor_stacks._wrap_executor_in_threading_stack(executor)
+
+
 def local_paillier_executor_factory(
     num_clients=None,
     num_client_executors=32,
     server_tf_device=None,
     aggregator_tf_device=None,
     client_tf_devices=tuple()):
-  # TODO consider parameterizing this function with channel_grid
-  channel_grid = channels.ChannelGrid({
-      (tff.CLIENTS,
-       paillier_placement.PAILLIER): channels.PlaintextChannel,
-      (tff.CLIENTS, 
-       tff.SERVER): channels.PlaintextChannel,
-      (paillier_placement.PAILLIER, 
-       tff.SERVER): channels.PlaintextChannel})
-  def intrinsic_strategy_fn(executor):
-    return paillier_strategy.PaillierStrategy(executor, channel_grid,
-        paillier_comp.make_keygen(bitlength=2048))
-
-  device_scheduler = _AggregatorDeviceScheduler(
-      server_tf_device, aggregator_tf_device, client_tf_devices)
-  stack_func = functools.partial(
-      _create_paillier_federated_stack,
-      num_client_executors=num_client_executors,
-      aggregator_device_scheduler=device_scheduler,
-      intrinsic_strategy_fn=intrinsic_strategy_fn)
-  if num_clients is None:
-    return _create_inferred_cardinality_factory(
-        stack_func)
-  return _create_explicit_cardinality_factory(
-      num_clients, stack_func)
-
-def _create_explicit_cardinality_factory(
-    num_clients, stack_func):
-  def _make_factory(cardinalities):
-    num_requested_clients = cardinalities.get(tff.CLIENTS)
-    if num_requested_clients is not None and num_requested_clients != num_clients:
-      raise ValueError('Expected to construct an executor with {} clients, '
-                       'but executor is hardcoded for {}'.format(
-                           num_requested_clients, num_clients))
-    return stack_func(num_clients)
-
-  return tff.framework.create_executor_factory(_make_factory)
-
-
-def _create_inferred_cardinality_factory(stack_func):
-  def _make_factory(cardinalities):
-    py_typecheck.check_type(cardinalities, dict)
-    for k, v in cardinalities.items():
-      py_typecheck.check_type(k, placement_literals.PlacementLiteral)
-      if k not in [tff.CLIENTS, tff.SERVER, paillier_placement.PAILLIER]:
-        raise ValueError('Unsupported placement: {}.'.format(k))
-      if v <= 0:
-        raise ValueError(
-            'Cardinality must be at '
-            'least one; you have passed {} for placement {}.'.format(v, k))
-    return stack_func(cardinalities.get(tff.CLIENTS, 0))
-
-  return tff.framework.create_executor_factory(_make_factory)
-
-
-def _create_paillier_federated_stack(num_clients, num_client_executors,
-    aggregator_device_scheduler, intrinsic_strategy_fn):
-  client_bottom_stacks = [
-      executor_stacks._create_bottom_stack(
-          device=aggregator_device_scheduler.next_client_device())
-      for _ in range(num_client_executors)]
-  executor_dict = {
-      tff.CLIENTS: [
-          client_bottom_stacks[k % len(client_bottom_stacks)]
-          for k in range(num_clients)],
-      tff.SERVER: executor_stacks._create_bottom_stack(
-          device=aggregator_device_scheduler.server_device()),
-      paillier_placement.PAILLIER: executor_stacks._create_bottom_stack(
-          device=aggregator_device_scheduler.aggregator_device()),
-      None: executor_stacks._create_bottom_stack(
-          device=aggregator_device_scheduler.server_device())}
-  return executor_stacks._complete_stack(
-      tff.framework.FederatingExecutor(
-          executor_dict, intrinsic_strategy_fn=intrinsic_strategy_fn))
-
-
-class _AggregatorDeviceScheduler(executor_stacks._DeviceScheduler):
-  """Assign server and clients to devices. Useful in multi-GPU environment."""
-
-  def __init__(self, server_tf_device, aggregator_tf_device, client_tf_devices):
-    """Initialize with server and client TF device placement.
-
-    Args:
-      server_tf_device: A `tf.config.LogicalDevice` to place server and other
-        computation without explicit TFF placement.
-      aggregator_tf_device: A `tf.config.LogicalDevice` to place Paillier
-        aggregator computations. This is currently required to be CPU device
-        by the Paillier primitives.
-      client_tf_devices: List/tuple of `tf.config.LogicalDevice` to place
-        clients for simulation. Possibly accelerators returned by
-        `tf.config.list_logical_devices()`.
-    """
-    super().__init__(server_tf_device, client_tf_devices)
-    if aggregator_tf_device is None:
-      self._aggregator_device = None
-    else:
-      py_typecheck.check_type(aggregator_tf_device, tf.config.LogicalDevice)
-      self._aggregator_device = aggregator_tf_device.name
+  """Like tff.framework.local_executor_factory, but with Paillier aggregation.
   
-  def aggregator_device(self):
-    return self._aggregator_device
+  The resulting factory function does not implement composing executor stacks,
+  so there is no max_fanout argument.
+
+  Args:
+    num_clients: The number of clients. If specified, the executor factory
+      function returned by `local_paillier_executor_factory` will be configured
+      to have exactly `num_clients` clients. If unspecified (`None`), then the
+      function returned will attempt to infer cardinalities of all placements
+      for which it is passed values.
+    num_client_executors: The number of distinct client executors to run
+      concurrently; executing more clients than this number results in
+      multiple clients having their work pinned on a single executor in a
+      synchronous fashion.
+    server_tf_device: A `tf.config.LogicalDevice` to place server and
+      other computation without explicit TFF placement.
+    aggregator_tf_device: A `tf.config.LogicalDevice` to place computation
+      of the Paillier aggregation. See README for a clearer description.
+    client_tf_devices: List/tuple of `tf.config.LogicalDevice` to place clients
+      for simulation. Possibly accelerators returned by
+      `tf.config.list_logical_devices()`.
+  """
+  # TODO consider parameterizing this function with channel_grid
+  if server_tf_device is not None:
+    py_typecheck.check_type(server_tf_device, tf.config.LogicalDevice)
+  py_typecheck.check_type(client_tf_devices, (tuple, list))
+  py_typecheck.check_type(num_client_executors, int)
+  if num_clients is not None:
+    py_typecheck.check_type(num_clients, int)
+  unplaced_ex_factory = AggregatingUnplacedExecutorFactory(
+      use_caching=True,
+      server_device=server_tf_device,
+      client_devices=client_tf_devices)
+  paillier_aggregating_executor_factory = PaillierAggregatingExecutorFactory(
+      num_client_executors=num_client_executors,
+      unplaced_ex_factory=unplaced_ex_factory,
+      num_clients=num_clients,
+      use_sizing=False)
+  factory_fn = paillier_aggregating_executor_factory.create_executor
+  return tff.framework.create_executor_factory(
+      paillier_aggregating_executor_factory.create_executor)
